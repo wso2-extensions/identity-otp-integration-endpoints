dd1f257ba840f15137faa5c9c733b08071ccd459
maneeshaindrachapa <maneesh.15@cse.mrt.ac.lk>
2021-08-09 21:34:33 +0530

    Auto generated files from openapi-tools.

---
+++ component/org.wso2.carbon.identity.api.otp.service.smsOtp/src/gen/java/org/wso2/identity/api/otp/service/smsotp/dto/OTPValidationResponse.java
@@ -0,0 +1,119 @@
+/*
+* Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package org.wso2.identity.api.otp.service.smsotp.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import javax.validation.constraints.*;
+
+
+import io.swagger.annotations.*;
+import java.util.Objects;
+import javax.validation.Valid;
+import javax.xml.bind.annotation.*;
+
+public class OTPValidationResponse  {
+  
+    private Boolean isValid;
+    private String userId;
+
+    /**
+    * SMS OTP validated successfully
+    **/
+    public OTPValidationResponse isValid(Boolean isValid) {
+
+        this.isValid = isValid;
+        return this;
+    }
+    
+    @ApiModelProperty(value = "SMS OTP validated successfully")
+    @JsonProperty("isValid")
+    @Valid
+    public Boolean getIsValid() {
+        return isValid;
+    }
+    public void setIsValid(Boolean isValid) {
+        this.isValid = isValid;
+    }
+
+    /**
+    * SCIM ID of the user which the token issued and validated
+    **/
+    public OTPValidationResponse userId(String userId) {
+
+        this.userId = userId;
+        return this;
+    }
+    
+    @ApiModelProperty(example = "8b1cc9c4-b671-448a-a334-5afe838a4a3b", value = "SCIM ID of the user which the token issued and validated")
+    @JsonProperty("userId")
+    @Valid
+    public String getUserId() {
+        return userId;
+    }
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+
+
+    @Override
+    public boolean equals(java.lang.Object o) {
+
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        OTPValidationResponse otPValidationResponse = (OTPValidationResponse) o;
+        return Objects.equals(this.isValid, otPValidationResponse.isValid) &&
+            Objects.equals(this.userId, otPValidationResponse.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(isValid, userId);
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("class OTPValidationResponse {\n");
+        
+        sb.append("    isValid: ").append(toIndentedString(isValid)).append("\n");
+        sb.append("    userId: ").append(toIndentedString(userId)).append("\n");
+        sb.append("}");
+        return sb.toString();
+    }
+
+    /**
+    * Convert the given object to string with each line indented by 4 spaces
+    * (except the first line).
+    */
+    private String toIndentedString(java.lang.Object o) {
+
+        if (o == null) {
+            return "null";
+        }
+        return o.toString().replace("\n", "\n");
+    }
+}
+
---
+++ component/org.wso2.carbon.identity.api.otp.service.smsOtp/src/main/java/org/wso2/identity/api/otp/service/smsotp/impl/SmsotpApiServiceImpl.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wso2.identity.api.otp.service.smsotp.impl;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.identity.smsotp.common.SMSOTPService;
+import org.wso2.carbon.identity.smsotp.common.SMSOTPServiceImpl;
+import org.wso2.carbon.identity.smsotp.common.dto.GenerationResponseDTO;
+import org.wso2.carbon.identity.smsotp.common.dto.ValidationResponseDTO;
+import org.wso2.carbon.identity.smsotp.common.exception.SMSOTPClientException;
+import org.wso2.carbon.identity.smsotp.common.exception.SMSOTPServerException;
+import org.wso2.identity.api.otp.service.smsotp.SmsotpApiService;
+import org.wso2.identity.api.otp.service.smsotp.dto.OTPGenerateResponse;
+import org.wso2.identity.api.otp.service.smsotp.dto.OTPGenerationRequest;
+import org.wso2.identity.api.otp.service.smsotp.dto.OTPValidationRequest;
+import org.wso2.identity.api.otp.service.smsotp.dto.OTPValidationResponse;
+import org.wso2.identity.api.otp.service.smsotp.utill.EndpointUtils;
+
+import javax.ws.rs.core.Response;
+
+/**
+ * This class implements the service layer for SmsotpAPI
+ */
+public class SmsotpApiServiceImpl implements SmsotpApiService {
+
+    private static final Log log = LogFactory.getLog(SmsotpApiServiceImpl.class);
+
+    @Override
+    public Response smsotpGeneratePost(OTPGenerationRequest otpGenerationRequest) {
+
+        try {
+            SMSOTPService smsOTPService = new SMSOTPServiceImpl();
+            GenerationResponseDTO responseDTO = smsOTPService.generateSMSOTP(otpGenerationRequest.getUserId());
+            OTPGenerateResponse response = new OTPGenerateResponse()
+                    .transactionId(responseDTO.getTransactionId())
+                    .smsOTP(responseDTO.getSmsOTP());
+            return Response.ok(response).build();
+        } catch (SMSOTPClientException e) {
+            return EndpointUtils.handleBadRequestResponse(e, log);
+        } catch (SMSOTPServerException e) {
+            return EndpointUtils.handleServerErrorResponse(e, log);
+        } catch (Throwable e) {
+            return EndpointUtils.handleUnexpectedServerError(e, log);
+        }
+    }
+
+    @Override
+    public Response smsotpValidatePost(OTPValidationRequest otpValidationRequest) {
+
+        try {
+            SMSOTPService smsOTPService = new SMSOTPServiceImpl();
+            ValidationResponseDTO responseDTO = smsOTPService.validateSMSOTP(
+                    otpValidationRequest.getTransactionId(),
+                    otpValidationRequest.getUserId(),
+                    otpValidationRequest.getSmsOTP()
+            );
+            OTPValidationResponse response = new OTPValidationResponse()
+                    .isValid(responseDTO.isValid())
+                    .userId(responseDTO.getUserId());
+            return Response.ok(response).build();
+        } catch (SMSOTPClientException e) {
+            return EndpointUtils.handleBadRequestResponse(e, log);
+        } catch (SMSOTPServerException e) {
+            return EndpointUtils.handleServerErrorResponse(e, log);
+        } catch (Throwable e) {
+            return EndpointUtils.handleUnexpectedServerError(e, log);
+        }
+    }
+}
