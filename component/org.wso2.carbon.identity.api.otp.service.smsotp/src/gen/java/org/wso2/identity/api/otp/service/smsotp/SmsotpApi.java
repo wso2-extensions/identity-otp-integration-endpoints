32ac1bc7b66df29a52d9cadabab3ba4c2936745a
maneeshaindrachapa <maneesh.15@cse.mrt.ac.lk>
2021-08-19 13:03:03 +0530

    Refactored license header and generated files.

--- component/org.wso2.carbon.identity.api.otp.service.smsotp/src/gen/java/org/wso2/identity/api/otp/service/smsotp/SmsotpApi.java
+++ component/org.wso2.carbon.identity.api.otp.service.smsotp/src/gen/java/org/wso2/identity/api/otp/service/smsotp/SmsotpApi.java
@@ -1,17 +1,19 @@
 /*
- * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.com).
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.wso2.identity.api.otp.service.smsotp;
@@ -49,7 +51,9 @@ public class SmsotpApi {
     @Path("/generate")
     @Consumes({"application/json"})
     @Produces({"application/json"})
-    @ApiOperation(value = "", notes = "This API is used to generate the smsotp. <b>Permission required:</b><br> * none<br> <b>Scope required:</b><br> * internal_login ", response = OTPGenerateResponse.class, authorizations = {
+    @ApiOperation(value = "", notes = "This API is used to generate the smsotp. <b>Permission required:</b><br>" +
+            " * none<br> <b>Scope required:</b><br> * internal_login ",
+            response = OTPGenerateResponse.class, authorizations = {
             @Authorization(value = "BasicAuth"),
             @Authorization(value = "OAuth2", scopes = {
 
@@ -62,7 +66,8 @@ public class SmsotpApi {
             @ApiResponse(code = 403, message = "Resource Forbidden", response = Void.class),
             @ApiResponse(code = 500, message = "Server Error", response = Error.class)
     })
-    public Response smsotpGeneratePost(@ApiParam(value = "", required = true) @Valid OTPGenerationRequest otPGenerationRequest) {
+    public Response smsotpGeneratePost(@ApiParam(value = "", required = true)
+                                       @Valid OTPGenerationRequest otPGenerationRequest) {
 
         return delegate.smsotpGeneratePost(otPGenerationRequest);
     }
@@ -72,7 +77,9 @@ public class SmsotpApi {
     @Path("/validate")
     @Consumes({"application/json"})
     @Produces({"application/json"})
-    @ApiOperation(value = "", notes = "This API is used to validate the smsotp. <b>Permission required:</b><br> * none<br> <b>Scope required:</b><br> * internal_login ", response = OTPValidationResponse.class, authorizations = {
+    @ApiOperation(value = "", notes = "This API is used to validate the smsotp. <b>Permission required:</b><br>" +
+            " * none<br> <b>Scope required:</b><br> * internal_login ",
+            response = OTPValidationResponse.class, authorizations = {
             @Authorization(value = "BasicAuth"),
             @Authorization(value = "OAuth2", scopes = {
 
@@ -85,7 +92,8 @@ public class SmsotpApi {
             @ApiResponse(code = 403, message = "Resource Forbidden", response = Void.class),
             @ApiResponse(code = 500, message = "Server Error", response = Error.class)
     })
-    public Response smsotpValidatePost(@ApiParam(value = "", required = true) @Valid OTPValidationRequest otPValidationRequest) {
+    public Response smsotpValidatePost(@ApiParam(value = "", required = true)
+                                       @Valid OTPValidationRequest otPValidationRequest) {
 
         return delegate.smsotpValidatePost(otPValidationRequest);
     }
--- component/org.wso2.carbon.identity.api.otp.service.smsotp/src/gen/java/org/wso2/identity/api/otp/service/smsotp/dto/OTPValidationResponse.java
+++ component/org.wso2.carbon.identity.api.otp.service.smsotp/src/gen/java/org/wso2/identity/api/otp/service/smsotp/dto/OTPValidationResponse.java
@@ -1,17 +1,19 @@
 /*
- * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.com).
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.wso2.identity.api.otp.service.smsotp.dto;
@@ -31,7 +33,9 @@ public class OTPValidationResponse {
     private String userId;
 
     /**
-     * SMS OTP validated successfully
+     * SMS OTP validated successfully.
+     *
+     * @param  isValid
      **/
     public OTPValidationResponse isValid(Boolean isValid) {
 
@@ -61,7 +65,8 @@ public class OTPValidationResponse {
         return this;
     }
 
-    @ApiModelProperty(example = "8b1cc9c4-b671-448a-a334-5afe838a4a3b", value = "SCIM ID of the user which the token issued and validated")
+    @ApiModelProperty(example = "8b1cc9c4-b671-448a-a334-5afe838a4a3b", value = "SCIM ID of the user which the token " +
+            "issued and validated")
     @JsonProperty("userId")
     @Valid
     public String getUserId() {
